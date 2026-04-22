2eb4b95bf4727bedb211f80e4c13ef5fd7c1ac91
SharanSShetty <165856813+SharanSShetty@users.noreply.github.com>
2026-04-22 14:33:38 +0530

    Fix requirements.txt encoding and blis version, and configure whitenoise for deployment

--- mini/settings.py
+++ mini/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = 'django-insecure-5(kvhblrtml7vzh0kg$(flnf6(!(r32(3+13=ire@r6=ke+(2q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -65,6 +65,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,6 +144,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
